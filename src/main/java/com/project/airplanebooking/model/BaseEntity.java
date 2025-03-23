a241018170e17e1d8939943f6bf7d48aa46596b8
tdthanh-dev <tdthanh.dev2025@gmail.com>
2025-03-23 14:12:39 +0700

    Domain model: Add base entity with common attributes

---
+++ src/main/java/com/project/airplanebooking/model/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.project.airplanebooking.model;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+
+@Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
